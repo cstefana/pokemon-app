38756b010ee0f061c32cc9f1ef21540bc0d4c9be
cstefana <stefana.ciudin@cognizant.com>
2026-03-24 11:46:33 +0200

    feat: moved pokemonlist to a separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,6 @@
-import { Link, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { PokemonDetail } from './components/PokemonDetail'
-import { usePokemon } from './hooks/usePokemon'
-import { getPokemonId } from './utils/pokemon'
-
-function PokemonList() {
-  const { pokemon, loading, error } = usePokemon()
-
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error: {error}</p>
-
-  return (
-    <main>
-      <h1>Pokemon</h1>
-      <ul>
-        {pokemon.map((item) => {
-          const id = getPokemonId(item.url)
-          return (
-            <li key={item.name}>
-              <Link to={`/pokemon/${id}`}>{item.name}</Link>
-            </li>
-          )
-        })}
-      </ul>
-    </main>
-  )
-}
+import { PokemonList } from './components/PokemonList'
 
 function App() {
   return (
